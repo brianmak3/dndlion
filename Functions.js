9d43fec3e243f48b41c17b7d0fa8ccc0520a9af3
brianmak3 <brianworkss@gmail.com>
2019-09-09 18:48:29 +0300

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,14 +1,8 @@
 const Notification = require('./classes/notifications')
-function formatAMPM(dateId) {
-    var time = new Date(dateId);
-    return time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-
-}
 function saveAndEmitNotification(data,comment, Commtype,broadcast){
    var now = new Date
     Notification.storeNotification(data.userId, {
         notfiedBy: data.ownerId,
-        time: formatAMPM(now),
         date: now.toString().substr(0,15),
         text:comment,
         id: now,
